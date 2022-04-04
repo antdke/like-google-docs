4862ef1287f6fb4e58f9f39717847b090cf8631e
Anthony Diké <anthonydike@nyu.edu>
2022-04-04 15:39:39 -0400

    installed Draft.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react'
+import {Editor, EditorState} from 'draft-js';
+import 'draft-js/dist/Draft.css';
+
+function MyEditor() {
+  const [editorState, setEditorState] = useState(
+    () => EditorState.createEmpty(),
+  );
+
+  return <Editor editorState={editorState} onChange={setEditorState} />;
+}
 
 function App() {
   return (
-    <h1 className="text-2xl font-bold underline">Hello World</h1>
+    <div>
+      <h1 className="text-2xl font-bold underline">Hello World</h1>
+      <MyEditor />
+    </div>
+    
   );
 }
 
