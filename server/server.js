0ebec6096ed44936a9f281d4302e01323ab09167
Anthony Diké <anthonydike@nyu.edu>
2022-04-09 13:34:52 -0400

    express (yourself) server

--- server/server.js
+++ server/server.js
@@ -1,5 +1,16 @@
-const io = require("socket.io")('3001',{
+const app = require('express')()
+const httpServer = require('http').createServer(app);
+const port = 3001
+const io = require("socket.io")(httpServer,{
   "cors": {origin: "http://localhost:3000", methods: ["GET", "POST"]}
+})
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+httpServer.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
 })
 
 io.on("connection", (socket) => {
